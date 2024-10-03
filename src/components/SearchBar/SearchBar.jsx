7e2f744f542076d2a94c7775132f4dc9213daafb
habil shuraimi <habilshuraimi9090@gmail.com>
2024-10-03 20:32:41 +0530

    final commit

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -6,7 +6,7 @@ const SearchBar = () => {
     <div className="search-container">
       <input type="text" className="search-input" placeholder="Search for titles..." />
       <button className="search-btn" type="button">
-        <i ></i> {/* Ensure you have Font Awesome imported */}
+        
       </button>
     </div>
   );
